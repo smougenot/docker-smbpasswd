f5a1f79d04b6d4455590bce51c591b6514e4f1b0
Sylvain Mougenot <sylvain.mougenot@gmail.com>
2023-02-22 18:43:41 +0100

    Fix path problem

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ set -o nounset    # fail on unset variables
 dir=$(cd -P -- "$(dirname -- "$BASH_SOURCE[0]")" && pwd -P)
 
 echo "Building image"
-docker build --file "${dir}/Dockerfile" --tag smbpasswd
+docker build --file "${dir}/Dockerfile" --tag smbpasswd "${dir}"
 
 _domain="172.17.4.23"
 _user="${USERNAME}"
